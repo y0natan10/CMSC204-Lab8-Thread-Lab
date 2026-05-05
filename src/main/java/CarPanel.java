b9552a085333730a890561ef09e58407e5181126
Yonatan Rubin <yonirubin2002@gmail.com>
2026-05-05 07:58:34 -0400

    cars no longer go out of bounds

--- src/main/java/CarPanel.java
+++ src/main/java/CarPanel.java
@@ -1,62 +1,76 @@
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+
 import javax.swing.JComponent;
 
 /**
-   This component draws two car shapes.
-*/
-public class CarPanel extends JComponent
-{  
+ * This component draws two car shapes.
+ */
+public class CarPanel extends JComponent {
 	private Car car1;
-	private int x,y, delay;
+	private int x, y, delay;
 	private CarQueue carQueue;
 	private int direction;
-	
-	CarPanel(int x1, int y1, int d, CarQueue queue)
-	{
-		delay = d;
-        x=x1;
-        y=y1;
-        car1 = new Car(x, y, this);
-        carQueue = queue;
-	}
-	public void startAnimation()
-	   {
-	      class AnimationRunnable implements Runnable
-	      {
-	         public void run()
-	         {
-	            try
-	            {
-	               for(int i=0;i<10;i++)
-	               {
-	            	   direction = carQueue.deleteQueue();
-	            	   x = x+10;
-	            	   repaint();
-	            	   Thread.sleep(delay*1000);
-	            	   
-	               }
-	            }
-	            catch (InterruptedException exception)
-	            {
-	            	
-	            }
-	            finally
-	            {
-	            	
-	            }
-	         }
-	      }
-	      
-	      Runnable r = new AnimationRunnable();
-	      Thread t = new Thread(r);
-	      t.start();
-	   }
-	
-   public void paintComponent(Graphics g)
-   {  
-      Graphics2D g2 = (Graphics2D) g;
 
-      car1.draw(g2,x,y);    
-   }
+	CarPanel(int x1, int y1, int d, CarQueue queue) {
+		delay = d;
+		x = x1;
+		y = y1;
+		car1 = new Car(x, y, this);
+		carQueue = queue;
+	}
+
+	public void startAnimation() {
+		class AnimationRunnable implements Runnable {
+			public void run() {
+				try {
+					for (int i = 0; i < 10; i++) {
+						direction = carQueue.deleteQueue();
+
+						if (direction == 0 && y <= 0)
+							direction = 1; // If going Up and at top, go Down
+						else if (direction == 1 && y >= getHeight() - 30)
+							direction = 0; // If going Down and at bottom, go Up
+						else if (direction == 2 && x >= getWidth() - 60)
+							direction = 3; // If going Right and at edge, go Left
+						else if (direction == 3 && x <= 0)
+							direction = 2; // If going Left and at edge, go Right
+
+						switch (direction) {
+						case 0:
+							y -= 10; // vertical controls are inverted for some reason (:
+							break;
+						case 1:
+							y += 10;
+							break;
+						case 2:
+							x += 10;
+							break;
+						case 3:
+							x -= 10;
+							break;
+						}
+
+						repaint();
+						Thread.sleep(delay * 1000);
+
+					}
+				} catch (InterruptedException exception) {
+
+				} finally {
+
+				}
+			}
+		}
+
+		Runnable r = new AnimationRunnable();
+		Thread t = new Thread(r);
+		t.start();
+	}
+
+	public void paintComponent(Graphics g) {
+		Graphics2D g2 = (Graphics2D) g;
+
+		car1.draw(g2, x, y);
+	}
 }
